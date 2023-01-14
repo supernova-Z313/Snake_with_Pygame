5dec987d2ff82bfb70357e75e5af9ab096c83aa3
Supernova <supernova313.313z@gmail.com>
2023-01-14 06:54:58 +0330

    add walls to game and debug button problem

--- warmup/main.py
+++ warmup/main.py
@@ -12,7 +12,7 @@ pygame.font.init()
 # ======================================================
 # init Constant
 
-FPS = 30
+FPS = 50
 WITHE = (255, 255, 255)
 BLACK_M = (18, 18, 18)
 PERPEL = (255, 0, 255)
@@ -35,6 +35,7 @@ apple_5 = pygame.image.load("./Assets/5.png")
 apple_10 = pygame.image.load("./Assets/10.png")
 apple_15 = pygame.image.load("./Assets/15.png")
 button = pygame.image.load("./Assets/button.png")
+break_b = pygame.image.load("./Assets/break.png")
 setting_score = pygame.font.SysFont("comicsans", 20, bold=True, italic=False)
 setting_finish = pygame.font.SysFont("comicsans", 50, bold=True, italic=False)
 setting_reset = pygame.font.SysFont("comicsans", 25, bold=False, italic=False)
@@ -89,15 +90,15 @@ def playing(state, score, clock): # _again_and_state_and_score
 # ======================================================
 # handleing movment
 
-def handel_movment(keys_pressed, last_direction):
+def handel_movment(keys_pressed, last_direction, real_direction):
 	# also can add big char
-	if keys_pressed[pygame.K_d] and last_direction != 3:
+	if keys_pressed[pygame.K_d] and real_direction != 3:
 		last_direction = 1
-	elif keys_pressed[pygame.K_s] and last_direction != 4:
+	elif keys_pressed[pygame.K_s] and real_direction != 4:
 		last_direction = 2
-	elif keys_pressed[pygame.K_a] and last_direction != 1:
+	elif keys_pressed[pygame.K_a] and real_direction != 1:
 		last_direction = 3
-	elif keys_pressed[pygame.K_w] and last_direction != 2:
+	elif keys_pressed[pygame.K_w] and real_direction != 2:
 		last_direction = 4
 
 	return last_direction
@@ -105,7 +106,7 @@ def handel_movment(keys_pressed, last_direction):
 # ======================================================
 # update window func
 
-def draw_game_win(head, body, apple, score, best_score_saved):
+def draw_game_win(head, body, apple, score, best_score_saved, walls):
 	win.fill(WITHE)
 	win.blit(top_1, (0, 0))
 	win.blit(top_2, (0, 93))
@@ -120,6 +121,9 @@ def draw_game_win(head, body, apple, score, best_score_saved):
 		win.blit(horizontal_line, (0, 100+(50*i)-1))
 	for i in range(11):
 		win.blit(vertical_line, ((50*i)-1, 100))
+
+	for i in walls:
+		win.blit(break_b, (i[0]-23, i[1]-23))
 
 	for i in body:
 		win.blit(snake_body, (i[0]-23, i[1]-23))  # (2, 102)
@@ -138,11 +142,13 @@ def draw_game_win(head, body, apple, score, best_score_saved):
 # ======================================================
 # new place of apple
 
-def new_apple(game_map, body, head): # walls,
+def new_apple(game_map, body, head, walls): # walls,
 	game_map_c = game_map.copy()
 	for i in body:
 		game_map_c.remove(i)
 	for i in head:
+		game_map_c.remove(i)
+	for i in walls:
 		game_map_c.remove(i)
 
 	result_apple = choice(game_map_c)
@@ -170,17 +176,19 @@ def main():
 			else:
 				best_score_saved = int(best_score_saved)
 
-		walls = []
+		walls = [[25, 375], [75, 375], [125, 375], [325, 275], [325, 325], [325, 375], [325, 425], [325, 475]]
+
+		border = []
 		for x in range(12):
 		    for y in range(12):
 		        if 0 < 50*x-25 < 500 and 100 < 50*y+75 < 600:
 		            continue
-		        walls.append([50*x-25, 50*y+75])
+		        border.append([50*x-25, 50*y+75])
 
 		head = [75, 125]
 		body = [[25, 125]]
 		head_c = head.copy()
-		apple = new_apple(game_map, body, [head])
+		apple = new_apple(game_map, body, [head], walls)
 		add_to_end = False
 		real_direction = 1
 		user_direction = 1
@@ -206,7 +214,7 @@ def main():
 
 
 			keys_pressed = pygame.key.get_pressed()
-			user_direction = handel_movment(keys_pressed, user_direction)
+			user_direction = handel_movment(keys_pressed, user_direction, real_direction)
 
 			fps_level += 1
 			if fps_level == 20:
@@ -223,7 +231,7 @@ def main():
 				else:
 					head_c[1] -= 50
 
-				if head_c in walls or head_c in body:
+				if head_c in border or head_c in body or head_c in walls:
 					run = False
 					break
 
@@ -235,7 +243,7 @@ def main():
 						run = False
 						state = 1
 						break
-					apple = new_apple(game_map, body, [head, head_c])
+					apple = new_apple(game_map, body, [head, head_c], walls)
 
 			# move body
 			if move:
@@ -273,7 +281,7 @@ def main():
 				else:
 					move_counter += 1
 
-			draw_game_win(head, body, apple, score, best_score_saved)
+			draw_game_win(head, body, apple, score, best_score_saved, walls)
 
 		if game:
 			game = playing(state, score, clock)
